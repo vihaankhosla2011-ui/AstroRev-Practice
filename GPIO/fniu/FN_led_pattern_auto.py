8975d4f1d7fd8982b7cb0ae2fb761a06ce8750a8
binnyism <kristiuskat@gmail.com>
2025-12-11 10:43:09 -0800

    Accepting Fangyi's work in progress for LED sequences; correctness and quality are not verified.
    * Create FN_led_on_off_auto.py
    
    Created my own while loop with 3 lights flashing in sequence
    
    * Create FN_led_pattern_auto.py
    
    Created new LED light pattern each on for one second each following pattern in video

---
+++ GPIO/fniu/FN_led_pattern_auto.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+# *****************************************************************************
+# Raspberry Pi Python program to toggle one LED on/off indefinitely.
+# *****************************************************************************
+# Wolf Witt, 2025-12-03
+# *****************************************************************************
+
+# GPIO Control Library Reference
+# ------------------------------
+# https://pypi.org/project/rpi-lgpio/
+# https://rpi-lgpio.readthedocs.io/en/latest/
+# https://rpi-lgpio.readthedocs.io/en/latest/api.html
+
+#red is 1.8V
+# yellow is 2.1V
+# gren is 2.2 V
+
+# *** Import Libraries ***
+
+import RPi.GPIO as GPIO
+import time
+
+
+# *** Set Up Pin Assignments ***
+
+GPIO.setmode ( GPIO.BOARD )  # use header pin numbers, not GPIO numbers
+
+ledPinRed = 15  # GPIO 22
+ledPinYellow = 16 #GPIO 23
+ledPinGreen = 18 #GPIO 24
+
+
+# *** Set Up Pin Behavior Modes *** 
+
+GPIO.setup ( ledPinRed, GPIO.OUT )  # set pin as output
+GPIO.setup ( ledPinYellow, GPIO.OUT )
+GPIO.setup ( ledPinGreen, GPIO.OUT )
+# *** Drive Pins ***
+ GPIO.output ( ledPinRed, GPIO.LOW  )  # drive pin low  (off, 0V)
+ GPIO.output ( ledPinYellow, GPIO.LOW  )  # drive pin low  (off, 0V)
+ GPIO.output ( ledPinGreen, GPIO.LOW  )  # drive pin low  (off, 0V)
+
+while True:
+    # green 1 second
+    GPIO.output ( ledPinGreen, GPIO.HIGH  )  
+    time.sleep ( 1 )  # wait 1 seconds
+    GPIO.output ( ledPinGreen, GPIO.LOW  )  
+    #yellow 1 and then yellow and green 1
+    GPIO.output ( ledPinYellow, GPIO.HIGH )  
+    time.sleep ( 1 )  # wait 1 seconds
+    GPIO.output ( ledPinGreen, GPIO.HIGH  )  
+    time.sleep ( 1 )  # wait 1 seconds
+    GPIO.output ( ledPinGreen, GPIO.LOW  )  
+    GPIO.output ( ledPinYellow, GPIO.LOW  )  
+  #red 1 and then red and green 1
+    GPIO.output ( ledPinRed, GPIO.HIGH  )  
+    time.sleep ( 1 )  # wait 1 seconds
+    GPIO.output ( ledPinGreen, GPIO.HIGH  )
+    time.sleep ( 1 )  # wait 1 seconds
+    GPIO.output ( ledPinGreen, GPIO.LOW  )
+    GPIO.output ( ledPinYellow, GPIO.HIGH  )
+    time.sleep ( 1 )  # wait 1 seconds
+    GPIO.output ( ledPinGreen, GPIO.HIGH  )
+    time.sleep ( 1 )  # wait 1 seconds
+  #close all
+    GPIO.output ( ledPinRed, GPIO.LOW  )  # drive pin low  (off, 0V)
+    GPIO.output ( ledPinYellow, GPIO.LOW  )  # drive pin low  (off, 0V)
+    GPIO.output ( ledPinGreen, GPIO.LOW  )  # drive pin low  (off, 0V)
